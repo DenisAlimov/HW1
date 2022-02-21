dad9892110140c17624503cdbbabb1afcb0dacc2
Denis Alimov <de.alimov@mail.ru>
2022-02-21 08:48:02 +0300

    Добавлен код домашнего задания и исправлена ошибка для корректной компиляции

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y.equals(0) ? 0 : x / y; //Ошибка была в делении на 0. При таком событии калькулятор будет выводить "0". Также есть вариант решения в виде использования всех переменных типа Double для возврата значения "Infinity".
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
